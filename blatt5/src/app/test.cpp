183437795d52e3eb1e9846a29e30aa9c0ab8cf76
Rayen Labidi <89748200+IamRayen@users.noreply.github.com>
2024-08-15 13:20:33 +0200

    Aufgabe 1 und 2 Operator friend-Declaration and Template classes

---
+++ blatt5/src/app/test.cpp
@@ -0,0 +1,104 @@
+// TODO:
+//  Entfernt die Kommentar-Zeichen (//) für Aufgaben, die erledigt sind und getestet werden sollen:
+
+
+#define TEST_AUFGABE_1 // TODO Aufgabe 1
+//#define TEST_AUFGABE_3 // TODO Aufgabe 3
+
+
+#include "test.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <string>
+using std::string;
+#include <sstream>
+using std::stringstream;
+#include <algorithm>
+using std::count;
+
+#include "Coordinates.h"
+using Sea::Coordinates;
+
+#ifdef TEST_AUFGABE_3
+#include "Grid2D.h"
+using Sea::Grid2D;
+#endif
+
+#include "PlayerSea.h"
+using GameObjects::PlayerSea;
+#include "Constants.h"
+using GameObjects::Constants;
+
+#include "utils.h"
+using Utils::n_endl;
+
+
+bool testPassed()
+{
+    bool testResult =
+            testOperator() &&
+            testGrid2D();
+
+    if (testResult) {
+        cout << "Test OK." << n_endl(2);
+        return true;
+    }
+    cout << "Test fehlgeschlagen." << n_endl(2);
+    return false;
+}
+
+bool testOperator()
+{
+#ifdef TEST_AUFGABE_1
+    stringstream stream;
+    stream << PlayerSea("Testspieler");
+
+    auto str = stream.str();
+    return
+        str.find("Testspieler") != string::npos &&
+                (unsigned int)(count(str.begin(), str.end(), '~')) == (2 * Constants::seaSizeX * Constants::seaSizeY);
+#else
+    return true;
+#endif
+}
+
+bool testGrid2D()
+{
+#ifdef TEST_AUFGABE_3
+    Grid2D<int> grid1(2, 2, 123'456);
+    Coordinates c1(0, 1);
+    Coordinates c2(1, 0);
+
+    grid1(1, 0) = 7;
+    grid1[c1] = 8;
+    grid1[{1, 1}] = 9;
+
+    bool testInt =
+            grid1(0, 0) == 123'456 &&
+            grid1(0, 1) == grid1[c1] &&
+            grid1[c2] == 7 &&
+            grid1[{0, 1}] == 8;
+
+
+    Grid2D<Coordinates> grid2(3, 3, Coordinates(123, 456));
+    grid2(2, 2) = Coordinates(76, 54);
+    bool testExceptions = false;
+    try {
+        grid2(3, 3) = Coordinates(32, 10);
+    }
+    catch (...) {
+        testExceptions = true;
+    }
+
+    bool testCoordinates =
+            grid2(1, 1).getX() == 123 &&
+            grid2(2, 2).getY() == 54;
+
+
+    return testInt && testExceptions && testCoordinates;
+#else
+    return true;
+#endif
+}
---
+++ blatt5/src/libSea/Grid2D.inl
@@ -0,0 +1,30 @@
+
+// Siehe Hinweis in 'game.inl'
+
+#include <exception>
+
+
+
+// TODO Aufgabe 3:
+//  Implementiert den Konstruktor von Grid2D.
+//  Stellt mithilfe von Exceptions sicher, dass kein Grid2D mit einer ungültigen Größe (< 1x1) erstellt werden kann.
+//  Tipp: Zur Initialisierung stellt `std::vector` einen entsprechenden Konstruktor zur Verfügung: `std::vector<ElementTyp>(Anzahl, initElement)`
+//        Dieser wurde auch schon für `gridOwnSea` in 'PlayerSea.h' verwendet.
+/*
+ ???
+ */
+
+// TODO Aufgabe 3:
+//  Implementiert `sizeX()` und `sizeY()`.
+//  Erinnerung: die Größe in X-Richtung entspricht dabei der Anzahl der Elemente (vom Typ `T`) in den "inneren" Vektoren.
+//                           Y-Richtung ist die Größe des "außeren" Vektors `grid`.
+/*
+ ???
+ */
+
+// TODO Aufgabe 3:
+//  Implementiert die 4 Operator-Überladungen.
+//  Stellt sicher, dass Exceptions geworfen werden, wenn ungültige Koordinaten übergeben wurden.
+/*
+ ???
+ */
